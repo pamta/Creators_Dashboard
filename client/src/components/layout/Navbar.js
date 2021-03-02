519fe7d10e37e5d56f039a3c35fb2e7be1ff7343
AlePestana <marialepestana@gmail.com>
2021-03-02 16:10:09 -0600

    Create basic NavbarLink component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,3 +1,4 @@
+import NavbarLink from './NavbarLink'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
@@ -8,11 +9,21 @@ const Navbar = () => {
 					<h1 className='font-semibold text-xl text-center mb-4'>
 						<Link to='/'>Creator's Dashboard</Link>
 					</h1>
-					<div>
-						<a>
-							<Link to='/posts'>Posts</Link>
-						</a>
-					</div>
+					<NavbarLink
+						name='Posts'
+						link='/posts'
+						icon={
+							<svg
+								class='w-5 h-5'
+								fill='currentColor'
+								viewBox='0 0 20 20'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path d='M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z'></path>
+							</svg>
+						}
+						color='green'
+					/>
 					<div>
 						<a>
 							<Link to='/analytics'>Analytics</Link>
