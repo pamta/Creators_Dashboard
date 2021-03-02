4a19bfe1a225673c8d7f1cc2ca91b0eb17e334ab
AlePestana <marialepestana@gmail.com>
2021-03-02 17:21:27 -0600

    Add navbar links for main actions

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,13 +5,44 @@ const Navbar = () => {
 	return (
 		<div className='w-72 p-4 bg-gray-100'>
 			<div className='flex flex-col justify-between h-full'>
-				<div>
+				<div className='flex flex-col space-y-2'>
 					<h1 className='font-semibold text-xl text-center mb-4'>
 						<Link to='/'>Creator's Dashboard</Link>
 					</h1>
 					<NavbarLink
 						name='Posts'
 						link='/posts'
+						icon={
+							<svg
+								class='w-6 h-6'
+								fill='currentColor'
+								viewBox='0 0 20 20'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path d='M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z'></path>
+								<path d='M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z'></path>
+							</svg>
+						}
+						color='green'
+					/>
+					<NavbarLink
+						name='Analytics'
+						link='/analytics'
+						icon={
+							<svg
+								class='w-6 h-6'
+								fill='currentColor'
+								viewBox='0 0 20 20'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<path d='M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z'></path>
+							</svg>
+						}
+						color='red'
+					/>
+					<NavbarLink
+						name='Notes'
+						link='/notes'
 						icon={
 							<svg
 								class='w-5 h-5'
@@ -22,18 +53,8 @@ const Navbar = () => {
 								<path d='M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z'></path>
 							</svg>
 						}
-						color='green'
+						color='yellow'
 					/>
-					<div>
-						<a>
-							<Link to='/analytics'>Analytics</Link>
-						</a>
-					</div>
-					<div>
-						<a>
-							<Link to='/notes'>Notes</Link>
-						</a>
-					</div>
 				</div>
 				<div>
 					<div>
