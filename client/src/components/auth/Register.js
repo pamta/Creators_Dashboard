f27418210728ebca55049bd8437a672f5b7e1c4d
HumbertoGlez <hglez.98s@gmail.com>
2021-03-03 14:40:32 -0600

    Unified design of login and sign up page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -21,58 +21,72 @@ const Register = ({title}) => {
                             {title}
                         </div>
                         <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-                            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-                                <div className="pt-4 text-gray-500 text-center mb-3 font-bold">
-                                    <small>Sign Up</small>
+                                <div className="pt-4 text-gray-600 text-center mb-3 font-bold">
+                                    Sign Up
                                 </div>
-                                <form>
-                                    <div className="relative w-full mb-3">
-                                        <input
-                                        type="email"
-                                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                                        placeholder="Email Address"
-                                        name="email"
-                                        required
-                                        />
-                                    </div>
-                                    <div className="relative w-full mb-3">
-                                        <input
-                                        placeholder="Username"
-                                        name="username"
-                                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                                        required
-                                        />
-                                    </div>
-                                    <div className="relative w-full mb-3">
-                                        <input
-                                        type="password"
-                                        placeholder="Password"
-                                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                                        name="paswword"
-                                        minLength="6"
-                                        required
-                                        />
-                                    </div>
-                                    <div className="relative w-full mb-3">
-                                        <input 
-                                        type="password"
-                                        placeholder="Confrim Password"
-                                        name="password2"
-                                        className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                                        minLength="6"
-                                        required
-                                        />
-                                    </div>
-                                    <div className="text-center mt-6">
-                                        <input
-                                        type="submit"
-                                        value="Register"
-                                        className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                                        />
-                                    </div>
-                                </form>
+                                <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                                    <form>
+                                        <div className="relative w-full mb-3">
+                                            <label className="block uppercase text-gray-700 text-xs font-bold mb-2">
+                                                Email Address
+                                            </label>
+                                            <input
+                                            type="email"
+                                            className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                                            placeholder="Email Address"
+                                            name="email"
+                                            required
+                                            />
+                                        </div>
+                                        <div className="relative w-full mb-3">
+                                            <label className="block uppercase text-gray-700 text-xs font-bold mb-2">
+                                                Username
+                                            </label>
+                                            <input
+                                            placeholder="Username"
+                                            name="username"
+                                            className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                                            required
+                                            />
+                                        </div>
+                                        <div className="relative w-full mb-3">
+                                            <label className="block uppercase text-gray-700 text-xs font-bold mb-2">
+                                                Password
+                                            </label>
+                                            <input
+                                            type="password"
+                                            placeholder="Password"
+                                            className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                                            name="paswword"
+                                            minLength="6"
+                                            required
+                                            />
+                                        </div>
+                                        <div className="relative w-full mb-3">
+                                            <label className="block uppercase text-gray-700 text-xs font-bold mb-2">
+                                                Confirm Password
+                                            </label>
+                                            <input 
+                                            type="password"
+                                            placeholder="Confirm Password"
+                                            name="password2"
+                                            className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                                            minLength="6"
+                                            required
+                                            />
+                                        </div>
+                                        <div className="text-center mt-6">
+                                            <button
+                                            className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                                            type="submit"
+                                            style={{ transition: "all .15s ease" }}
+                                            >
+                                                Register
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
-                        </div>
                         </div>
                     </div>
                 </div>
