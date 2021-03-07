5bae666239853798f14d3a6d26668d1637032604
AlePestana <marialepestana@gmail.com>
2021-03-07 17:14:18 -0600

    Add handler button on user settings

--- client/src/components/settings/UserSettingsFields.js
+++ client/src/components/settings/UserSettingsFields.js
@@ -2,7 +2,7 @@ import SimpleSettingsField from './SimpleSettingsField'
 
 const UserFields = () => {
 	return (
-		<div className='space-y-2'>
+		<div className='space-y-4'>
 			<SimpleSettingsField fieldName='Name' inputTag='name' />
 			<SimpleSettingsField fieldName='Last Name' inputTag='last-name' />
 		</div>
