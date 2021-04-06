a85925e206dc972175932db4552d3d34cb69b967
AlePestana <marialepestana@gmail.com>
2021-04-06 09:50:08 -0500

    Create basic social auth component

--- client/src/components/posts/NewPostPage.js
+++ client/src/components/posts/NewPostPage.js
@@ -21,6 +21,9 @@ const NewPostPage = () => {
 	const [content, setContent] = useState('')
 	const [fileNames, setFileNames] = useState([])
 	const [videoName, setVideoName] = useState('')
+	const [isYoutubeSelected, setYoutubeSelected] = useState(false)
+
+	console.log('is ? ' + (isYoutubeSelected ? 'yes' : 'no'))
 
 	return (
 		<div className='p-4'>
@@ -137,11 +140,61 @@ const NewPostPage = () => {
 				{/* Social auth component */}
 				<div
 					className={
-						'flex flex-col space-y-4 bg-gray-200 rounded-md p-6' +
-						(isTablet ? ' w-full' : ' w-72')
+						'flex flex-col space-y-4' + (isTablet ? ' w-full' : ' w-72')
 					}
 				>
-					<p>hi</p>
+					{/* YouTube */}
+					<div className='flex flex-col space-y-4 bg-gray-200 rounded-md p-6'>
+						<div className='flex flex-row space-x-2 items-center'>
+							<button
+								className='w-5 h-5 bg-white flex items-center justify-center rounded-xl cursor-pointer'
+								onClick={() => setYoutubeSelected(!isYoutubeSelected)}
+							>
+								{isYoutubeSelected ? (
+									<svg
+										className='w-4 h-4'
+										fill='none'
+										stroke='green'
+										viewBox='0 0 24 24'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+										></path>
+									</svg>
+								) : (
+									<svg
+										className='w-4 h-4'
+										fill='none'
+										stroke='red'
+										viewBox='0 0 24 24'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+										></path>
+									</svg>
+								)}
+							</button>
+							<svg
+								className='w-5 h-5'
+								role='img'
+								viewBox='0 0 24 24'
+								xmlns='http://www.w3.org/2000/svg'
+							>
+								<title>YouTube icon</title>
+								<path d='M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z' />
+							</svg>
+							<p>YouTube</p>
+						</div>
+						{isYoutubeSelected ? <p>selected</p> : <></>}
+					</div>
 				</div>
 			</div>
 
