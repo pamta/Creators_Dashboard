6f99eb5d5e6b2d4dd004fc6d222729686dc216c1
AlePestana <marialepestana@gmail.com>
2021-04-05 22:49:25 -0500

    Create upload file buttons

--- client/src/components/posts/NewPostPage.js
+++ client/src/components/posts/NewPostPage.js
@@ -4,27 +4,24 @@ import ytIcon from '../../assets/img/youtube_icon.png'
 import igIcon from '../../assets/img/instagram_icon.png'
 import useWindowSize from '../../lib/useWindowSize'
 import { useState } from 'react'
+import styles from './newPost.module.css'
 
 const NewPostPage = () => {
 	const isTablet = useWindowSize().width <= 1080
 
-	// const getLayoutStyle = () => {
-	// 	return isTablet
-	// 		? 'flex flex-col space-y-8 w-full'
-	// 		: 'flex flex-row space-x-8'
-	// }
-	const [title, setTitle] = useState('')
-	const [content, setContent] = useState('')
-
-	console.log('title')
-	console.log(title)
-	console.log(content)
-
 	const getLayoutStyle = () => {
 		return isTablet
 			? 'flex flex-col space-y-8 w-full'
-			: 'flex flex-col space-y-8 justify-between'
+			: 'flex flex-row space-x-8'
 	}
+
+	const [title, setTitle] = useState('')
+	const [content, setContent] = useState('')
+	const [fileNames, setFileNames] = useState([])
+	const [videoName, setVideoName] = useState('')
+
+	console.log('title')
+	console.log(title)
 
 	const getFieldStyle = () => {
 		return isTablet
@@ -78,79 +75,79 @@ const NewPostPage = () => {
 						/>
 					</div>
 				</div>
-			</div>
-			{/* <div className='flex-auto min-w-full shadow-lg rounded-lg px-5 py-5 bg-green-200'>
-				<form className='form text-gray-900 '>
-					<div className='flex flex-col w-full'>
-						<div className='flex py-2 justify-between'>
-							<div className='flex flex-row'>
-								<div className='px-3 py-3 text-xl md:text-2xl font-bold'>
-									Title:
-								</div>
-								<input
-									type='text'
-									className='px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-base md:text-xl shadow focus:outline-none focus:shadow-outline'
-									placeholder='Post Title'
-									style={{ transition: 'all .15s ease' }}
-								/>
-							</div>
-							<div className='flex flex-row'>
-								<div className='px-4 py-3 text-xl md:text-2xl font-bold'>
-									Platform:
-								</div>
-								<div className='flex items-center'>
-									<input type='checkbox' name='twitter' className='w-4 h-4' />
-									<img
-										className='pl-2 pr-4 w-12 md:w-16 h-auto'
-										src={twitterIcon}
-									/>
-									<input type='checkbox' name='facebook' className='w-4 h-4' />
-									<img className='pl-2 pr-4 w-12 md:w-16 h-auto' src={fbIcon} />
-									<input type='checkbox' name='youtube' className='w-4 h-4' />
-									<img className='pl-2 pr-4 w-12 md:w-16 h-auto' src={ytIcon} />
-									<input type='checkbox' name='instagram' className='w-4 h-4' />
-									<img className='pl-2 pr-4 w-12 md:w-16 h-auto' src={igIcon} />
-								</div>
-							</div>
-						</div>
-						<div className='px-3 py-3 text-xl md:text-2xl font-bold w-2/3'>
-							Content:
-						</div>
-						<textarea
-							required
-							className='px-3 py-3 resize-none w-2/3 placeholder-gray-400 text-gray-700 bg-white rounded text-base md:text-xl'
-							rows='20'
-							placeholder='My new post'
+
+				<div className={getLayoutStyle()}>
+					<label
+						htmlFor='slide'
+						className={'cursor-pointer' + (isTablet ? ' w-full' : ' w-1/2')}
+					>
+						<input
+							name='file'
+							type='file'
+							multiple
+							onChange={(e) => {
+								for (let i = 0; i < e.target.files.length; i++) {
+									setFileNames((prevFileNames) => [
+										...prevFileNames,
+										e.target.files[i].name,
+									])
+								}
+							}}
 						/>
-						<div className='flex flex-row py-3 items-center'>
-							<button className='flex flex-row px-3 py-3 justify-start items-center space-x-4 rounded-lg bg-green-600 text-black active:bg-green-400 text-base md:text-2xl shadow hover:shadow-lg hover:bg-green-700 hover:text-white'>
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									width='24'
-									height='24'
-									viewBox='0 0 24 24'
-									fill='none'
-									stroke='currentColor'
-									strokeWidth='2'
-									strokeLinecap='round'
-									strokeLinejoin='round'
-								>
-									<path d='M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3' />
-								</svg>
-								<p>Upload</p>
-							</button>
-							<div className='px-4 text-base md:text-xl text-blue-500'>
-								file1.png
-							</div>
+						<div className='flex flex-row px-3 py-3 justify-center items-center space-x-4 rounded-lg bg-green-200 text-black active:bg-green-400 text-md md:text-md shadow hover:shadow-lg hover:bg-green-400 hover:text-white'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								width='18'
+								height='18'
+								viewBox='0 0 24 24'
+								fill='none'
+								stroke='currentColor'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							>
+								<path d='M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3' />
+							</svg>
+							<p className='text-center'>Upload images</p>
 						</div>
-						<div className='flex justify-end'>
-							<button className=' px-10 py-5 rounded-lg bg-green-600 text-black active:bg-green-400 text-2xl md:text-4xl font-bold shadow hover:shadow-lg hover:bg-green-700 hover:text-white'>
-								Done
-							</button>
+						<div className='flex flex-col w-1/2 text-sm text-gray-500 italic'>
+							{fileNames.map((file) => (
+								<p>{file}</p>
+							))}
 						</div>
-					</div>
-				</form>
-			</div> */}
+					</label>
+
+					<label
+						htmlFor='video'
+						className={'cursor-pointer' + (isTablet ? ' w-full' : ' w-1/2')}
+					>
+						<input
+							name='file'
+							type='file'
+							onChange={(e) => setVideoName(e.target.value)}
+						/>
+						<div className='flex flex-row px-3 py-3 justify-center items-center space-x-4 rounded-lg bg-green-400 text-black active:bg-green-600 text-md md:text-md shadow hover:shadow-lg hover:bg-green-600 hover:text-white'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								width='18'
+								height='18'
+								viewBox='0 0 24 24'
+								fill='none'
+								stroke='currentColor'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							>
+								<path d='M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3' />
+							</svg>
+							<p className='text-center'>Upload video</p>
+						</div>
+						<div className='flex flex-col w-1/2 text-sm text-gray-500 italic'>
+							<p>{videoName.split('\\')[2]}</p>
+						</div>
+					</label>
+				</div>
+			</div>
 			<div className='py-5 float-right'>
 				<button className='p-2 text-sm md:text-sm rounded-lg bg-gray-400 text-black flex flex-row justify-center items-center space-x-2 hover:bg-gray-600 hover:text-white'>
 					<svg
