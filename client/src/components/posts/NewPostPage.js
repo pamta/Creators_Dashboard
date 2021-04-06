c615ef78459e0557373d6a1b0ac59c3139811deb
AlePestana <marialepestana@gmail.com>
2021-04-06 09:54:00 -0500

    Add Twitter and FB social auth components on new post page

--- client/src/components/posts/NewPostPage.js
+++ client/src/components/posts/NewPostPage.js
@@ -22,6 +22,8 @@ const NewPostPage = () => {
 	const [fileNames, setFileNames] = useState([])
 	const [videoName, setVideoName] = useState('')
 	const [isYoutubeSelected, setYoutubeSelected] = useState(false)
+	const [isFacebookSelected, setFacebookSelected] = useState(false)
+	const [isTwitterSelected, setTwitterSelected] = useState(false)
 
 	console.log('is ? ' + (isYoutubeSelected ? 'yes' : 'no'))
 
@@ -143,6 +145,111 @@ const NewPostPage = () => {
 						'flex flex-col space-y-4' + (isTablet ? ' w-full' : ' w-72')
 					}
 				>
+					{/* Twitter */}
+					<div className='flex flex-col space-y-4 bg-gray-200 rounded-md p-6'>
+						<div className='flex flex-row space-x-2 items-center'>
+							<button
+								className='w-5 h-5 bg-white flex items-center justify-center rounded-xl cursor-pointer'
+								onClick={() => setTwitterSelected(!isTwitterSelected)}
+							>
+								{isTwitterSelected ? (
+									<svg
+										className='w-4 h-4'
+										fill='none'
+										stroke='green'
+										viewBox='0 0 24 24'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+										></path>
+									</svg>
+								) : (
+									<svg
+										className='w-4 h-4'
+										fill='none'
+										stroke='red'
+										viewBox='0 0 24 24'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+										></path>
+									</svg>
+								)}
+							</button>
+							<svg
+								className='w-5 h-5'
+								role='img'
+								xmlns='http://www.w3.org/2000/svg'
+								viewBox='0 0 24 24'
+							>
+								<title>Twitter icon</title>
+								<path d='M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z' />
+							</svg>
+							<p>Twitter</p>
+						</div>
+						{isTwitterSelected ? <p>selected</p> : <></>}
+					</div>
+
+					{/* Facebook */}
+					<div className='flex flex-col space-y-4 bg-gray-200 rounded-md p-6'>
+						<div className='flex flex-row space-x-2 items-center'>
+							<button
+								className='w-5 h-5 bg-white flex items-center justify-center rounded-xl cursor-pointer'
+								onClick={() => setFacebookSelected(!isFacebookSelected)}
+							>
+								{isFacebookSelected ? (
+									<svg
+										className='w-4 h-4'
+										fill='none'
+										stroke='green'
+										viewBox='0 0 24 24'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+										></path>
+									</svg>
+								) : (
+									<svg
+										className='w-4 h-4'
+										fill='none'
+										stroke='red'
+										viewBox='0 0 24 24'
+										xmlns='http://www.w3.org/2000/svg'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											strokeWidth='2'
+											d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+										></path>
+									</svg>
+								)}
+							</button>
+							<svg
+								className='w-5 h-5'
+								role='img'
+								xmlns='http://www.w3.org/2000/svg'
+								viewBox='0 0 24 24'
+							>
+								<title>Facebook icon</title>
+								<path d='M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z' />
+							</svg>
+							<p>Facebook</p>
+						</div>
+						{isFacebookSelected ? <p>selected</p> : <></>}
+					</div>
 					{/* YouTube */}
 					<div className='flex flex-col space-y-4 bg-gray-200 rounded-md p-6'>
 						<div className='flex flex-row space-x-2 items-center'>
