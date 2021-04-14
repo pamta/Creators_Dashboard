820ad1fc97cabdbb5380e70b578b5dbadb8fa638
PabloMoreno2000 <pamta2000@gmail.com>
2021-04-14 16:32:00 -0500

    Deleted unused imports in route

--- modules/api/user/auth.js
+++ modules/api/user/auth.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const auth = require("../../../middleware/auth");
-const User = require("../../../models/User");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { check, validationResult } = require("express-validator");
 
+const auth = require("../../../middleware/auth");
 const UserService = require("./userService");
 const userService = new UserService();
 const userValidators = require("./userValidators");
