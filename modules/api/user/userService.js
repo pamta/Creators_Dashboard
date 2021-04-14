7c2304174fc71a0d904a9153b6d4601e2890c4b6
PabloMoreno2000 <pamta2000@gmail.com>
2021-04-14 15:07:24 -0500

    Created user service and its function to sign up a new user

--- modules/api/user/userService.js
+++ modules/api/user/userService.js
@@ -1,13 +1,54 @@
-const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const User = require("../../../models/User");
+const ArrayError = require("../../../utils/ArrayError");
 
 class UserService {
-  async Signup(userDTO) {
+  async signUp(userDTO) {
     const { name, userName, email, password } = userDTO;
-
+    // Check if there's a user with that email o that userName (since we put emails and usernames as unique)
     let mailFound = await User.findOne({ email }).exec();
     let userNameFound = await User.findOne({ userName }).exec();
     if (userNameFound || mailFound) {
-      return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      throw new ArrayError([{ msg: "User already exists" }]);
     }
+
+    //save current date in the created use. May use some other format so it is up to changes
+    const registrationDate = Date.now();
+    const updateDate = registrationDate;
+
+    const user = new User({
+      name,
+      email,
+      userName,
+      password,
+      registrationDate,
+      updateDate,
+    });
+
+    // 10 is recommended in documentation, the bigger the number means more security
+    const salt = await bcrypt.genSalt(10);
+
+    // // Encrypt the password
+    user.password = await bcrypt.hash(password, salt);
+    // // Remember that User is a moongose model and we connected moongose with our database
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id, // Moongose gets the id of the database
+      },
+    };
+
+    // // Generate a JSON Web Token (encrypted payload with signature)
+    const token = jwt.sign(
+      payload,
+      config.get("jwtSecret"), // encryption key
+      { expiresIn: 360000 }
+    );
+    return token;
   }
 }
+
+module.exports = UserService;
