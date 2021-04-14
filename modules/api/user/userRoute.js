dae413b8974e9838dff9acbe1ff9dec1d30b65a4
PabloMoreno2000 <pamta2000@gmail.com>
2021-04-14 15:59:21 -0500

    User update route uses service and validation layer

--- modules/api/user/userRoute.js
+++ modules/api/user/userRoute.js
@@ -34,73 +34,18 @@ router.post("/", userValidators.signUp, async (req, res) => {
 // @access Private/requires token
 // Passing the 'auth' middleware will execute the middleware function that will be executed before the callback.
 // Given a JSON web token, it updates a user if the token is valid and corresponds to the user id
-router.post(
-  "/update",
-  auth,
-  [
-    // Second parameter of check is a custom error message
-    // Checks for the value of a json key called "name"
-    check("name", "A name is required").not().isEmpty(),
-    check("userName", "A user name is required")
-      .not()
-      .isEmpty()
-      .not()
-      .isEmail(), //to prevent from erroniously use an email as user name
-    check("email", "Please include a valid email").isEmail(),
-  ],
-  async (req, res) => {
-    // The check is done with the second parameter of above within []
-    // Pass the request whose body has a json element
-
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      // 400 is for a bad request
-      return res.status(400).json({
-        errors: errors.array(),
-      });
+router.post("/update", auth, userValidators.update, async (req, res) => {
+  try {
+    userService.update(req.user.id, req.body);
+    res.send("User updated");
+  } catch (err) {
+    if (err.name == "ArrayError") {
+      return res.status(400).json({ errors: err.errors });
     }
-
-    //we get the alredy checked payload
-    const { name, userName, email } = req.body;
-
-    try {
-      //user exist in req because of the auth middleware
-      const tokenUser = req.user;
-
-      // Check if there's a user with that id
-      let userFound = await User.findById(tokenUser.id).exec();
-
-      if (!userFound) {
-        return res.status(400).json({ errors: [{ msg: "User non existent" }] });
-      }
-
-      const updateDate = Date.now();
-
-      await User.findByIdAndUpdate(
-        tokenUser.id,
-        {
-          name: name,
-          email: email,
-          userName: userName,
-          updateDate: updateDate,
-        },
-        (err, doc) => {
-          if (err) {
-            console.error(err.message);
-            res.status(500).send(`DB error: ${err}`);
-          }
-        }
-      ).exec();
-
-      res.send("User updated");
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Server error");
-    }
+    console.error(err);
+    res.status(500).send("Server error");
   }
-);
+});
 
 // @route  POST api/user/
 // @desct  Delete existing user profile
