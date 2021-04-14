99e65e874881285966837a2e83f2adb4833d109f
PabloMoreno2000 <pamta2000@gmail.com>
2021-04-14 16:06:32 -0500

    Now user delete route uses user service

--- modules/api/user/userRoute.js
+++ modules/api/user/userRoute.js
@@ -6,12 +6,6 @@ const UserService = require("./userService");
 const userValidators = require("./userValidators");
 const userService = new UserService();
 
-// Exporting two objects
-const { check, validationResult } = require("express-validator");
-
-// Returns a moongose model of the user
-const User = require("../../../models/User");
-
 // route to create a new user
 // @route  POST api/user
 // @desct  Register user
@@ -54,25 +48,12 @@ router.post("/update", auth, userValidators.update, async (req, res) => {
 // Given a JSON web token, will delete a user if the token is valid and corresponds to the user id
 router.delete("/", auth, async (req, res) => {
   try {
-    //user exist in req because of the auth middleware
-    const tokenUser = req.user;
-
-    // Check if there's a user with that id
-    let userFound = await User.findById(tokenUser.id).exec();
-
-    if (!userFound) {
-      return res.status(400).json({ errors: [{ msg: "User non existent" }] });
-    }
-
-    await User.remove({ _id: tokenUser.id }, (err, doc) => {
-      if (err) {
-        console.error(err.message);
-        res.status(500).send("DB error");
-      }
-    }).exec();
-
+    userService.delete(req.user.id);
     res.send("User deleted");
   } catch (err) {
+    if (err.name == "ArrayError") {
+      return res.status(400).json({ errors: err.errors });
+    }
     console.error(err);
     res.status(500).send("Server error");
   }
