3663e290cccbe1d459bad92ab2621d7d3a460961
PabloMoreno2000 <pamta2000@gmail.com>
2021-04-16 10:25:19 -0500

    Analytics of page was divided into it and post

--- models/AnalyticsFbPage.js
+++ models/AnalyticsFbPage.js
@@ -165,61 +165,6 @@ const AnalyticsFbPage = new mongoose.Schema({
       type: String,
     },
   },
-  pagePostEngagement: {
-    post_engaged_users: {
-      type: String,
-    },
-    post_negative_feedback: {
-      type: String,
-    },
-    post_negative_feedback_unique: {
-      type: String,
-    },
-    post_negative_feedback_by_type: {
-      type: String,
-    },
-    post_negative_feedback_by_type_unique: {
-      type: String,
-    },
-    post_engaged_fan: {
-      type: String,
-    },
-    post_clicks: {
-      type: String,
-    },
-    post_clicks_unique: {
-      type: String,
-    },
-    post_clicks_by_type: {
-      type: String,
-    },
-    post_clicks_by_type_unique: {
-      type: String,
-    },
-  },
-  pagePostReactions: {
-    post_reactions_like_total: {
-      type: String,
-    },
-    post_reactions_love_total: {
-      type: String,
-    },
-    post_reactions_wow_total: {
-      type: String,
-    },
-    post_reactions_haha_total: {
-      type: String,
-    },
-    post_reactions_sorry_total: {
-      type: String,
-    },
-    post_reactions_anger_total: {
-      type: String,
-    },
-    post_reactions_by_type_total: {
-      type: String,
-    },
-  },
 });
 
 module.exports = AnalyticsFbPage = mongoose.model(
