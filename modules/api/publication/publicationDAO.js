e096dccb46c37120d96f33eede1faed45a7cfc7e
AlePestana <marialepestana@gmail.com>
2021-04-20 10:45:05 -0500

    Create publicationDAO

---
+++ modules/api/publication/publicationDAO.js
@@ -0,0 +1,28 @@
+const Publication = require('../../../models/Publication')
+
+class PublicationDAO extends Publication {
+	constructor(publicationDTO) {
+		super(publicationDTO)
+	}
+
+	static find = async (jsonParams) => {
+		const publication = await Publication.find(jsonParams).exec()
+		return publication
+	}
+
+	static findOne = async (jsonParams) => {
+		const publication = await Publication.findOne(jsonParams).exec()
+		return publication
+	}
+
+	static remove = async (jsonParams, callback) => {
+		await Publication.remove(jsonParams, callback).exec()
+	}
+
+	upload = async () => {
+		await this.save()
+		return this
+	}
+}
+
+module.exports = PublicationDAO
