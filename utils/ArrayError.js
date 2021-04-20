90dcad3d0cc6515b49a85bda48c2a2906bd5179d
PabloMoreno2000 <pamta2000@gmail.com>
2021-04-20 13:47:41 -0500

    Fixed remaining git conflict

--- utils/ArrayError.js
+++ utils/ArrayError.js
@@ -1,5 +1,4 @@
 class ArrayError extends Error {
-<<<<<<< HEAD
   constructor(errorsArray, message = "") {
     super(message);
     this.errors = errorsArray;
@@ -8,13 +7,3 @@ class ArrayError extends Error {
 }
 
 module.exports = ArrayError;
-=======
-	constructor(errorsArray, message = '') {
-		super(message)
-		this.errors = errorsArray
-		this.name = 'ArrayError'
-	}
-}
-
-module.exports = ArrayError
->>>>>>> 5d61fcc621f04f55f52605c129e890821274ddb0
