d06c75c74dc8146fed1442b9be03c19d434325b1
PabloMoreno2000 <pamta2000@gmail.com>
2021-04-27 16:12:49 -0500

    Made static method to get the analytics of a post with their date

--- modules/api/publication/publicationDAO.js
+++ modules/api/publication/publicationDAO.js
@@ -1,36 +1,71 @@
-const Publication = require('../../../models/Publication')
+const Publication = require("../../../models/Publication");
+const SocialNetwork = require("../../../models/SocialNetwork");
+const fbAnalyticPost = require("../../../models/AnalyticsFbPost");
+
+const analyticByName = {
+  Facebook: fbAnalyticPost,
+};
 
 class PublicationDAO extends Publication {
-	constructor(publicationDTO) {
-		super(publicationDTO)
-	}
+  constructor(publicationDTO) {
+    super(publicationDTO);
+  }
 
-	static find = async (jsonParams) => {
-		const publication = await Publication.find(jsonParams).exec()
-		return publication
-	}
+  static find = async (jsonParams) => {
+    const publication = await Publication.find(jsonParams).exec();
+    return publication;
+  };
 
-	static findOne = async (jsonParams) => {
-		const publication = await Publication.findOne(jsonParams).exec()
-		return publication
-	}
+  static findOne = async (jsonParams) => {
+    const publication = await Publication.findOne(jsonParams).exec();
+    return publication;
+  };
 
-	static findByIdAndUpdate = async (publicationID, publication, callback) => {
-		await Publication.findByIdAndUpdate(
-			publicationID,
-			publication,
-			callback
-		).exec()
-	}
+  static findByIdAndUpdate = async (publicationID, publication, callback) => {
+    await Publication.findByIdAndUpdate(
+      publicationID,
+      publication,
+      callback
+    ).exec();
+  };
 
-	static remove = async (jsonParams, callback) => {
-		await Publication.remove(jsonParams, callback).exec()
-	}
+  static remove = async (jsonParams, callback) => {
+    await Publication.remove(jsonParams, callback).exec();
+  };
 
-	upload = async () => {
-		await this.save()
-		return this
-	}
+  static getPostAnalyticsByDate = async (jsonParams) => {
+    let publication = await Publication.findOne(jsonParams)
+      .populate("publicationsToSocialNetworks")
+      .exec();
+
+    const res = await Promise.all(
+      publication.publicationsToSocialNetworks.map(async (pub) => {
+        const socialNet = await SocialNetwork.findById(pub.socialNetwork);
+        const socialNetPostAnalytics = analyticByName[socialNet.name];
+
+        const arrAnalytics = [];
+        await Promise.all(
+          pub.analytics.map(async (analytic) => {
+            let element = await socialNetPostAnalytics.findById(
+              analytic.object
+            );
+            arrAnalytics.push({
+              date: analytic.date,
+              data: element,
+            });
+          })
+        );
+        return arrAnalytics;
+      })
+    );
+
+    return res;
+  };
+
+  upload = async () => {
+    await this.save();
+    return this;
+  };
 }
 
-module.exports = PublicationDAO
+module.exports = PublicationDAO;
