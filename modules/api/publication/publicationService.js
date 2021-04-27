11493f9101457bec8fbe94d756d215512addcc02
PabloMoreno2000 <pamta2000@gmail.com>
2021-04-27 16:13:46 -0500

    Used get post analytics of DAO in the server

--- modules/api/publication/publicationService.js
+++ modules/api/publication/publicationService.js
@@ -1,29 +1,39 @@
-const ArrayError = require('../../../utils/ArrayError')
-const Publication = require('./publicationDAO')
+const ArrayError = require("../../../utils/ArrayError");
+const Publication = require("./publicationDAO");
 
 class PublicationService {
-	async getAllPublicationsForUser(userID) {
-		const publications = await Publication.find({ user_id: userID })
-		if (!publications) {
-			throw new ArrayError([{ msg: 'No publications found for this user' }])
-		}
-		return publications
-	}
+  async getAllPublicationsForUser(userID) {
+    const publications = await Publication.find({ user_id: userID });
+    if (!publications) {
+      throw new ArrayError([{ msg: "No publications found for this user" }]);
+    }
+    return publications;
+  }
 
-	async getPublicationOfUser(publicationID, userID) {
-		const publication = await Publication.findOne({
-			_id: publicationID,
-			user_id: userID,
-		})
-		if (!publication) {
-			throw new ArrayError([{ msg: 'No publications found for this user' }])
-		}
-		return publication
-	}
+  async getPublicationOfUser(publicationID, userID) {
+    const publication = await Publication.findOne({
+      _id: publicationID,
+      user_id: userID,
+    });
 
-	async updatePublication(publicationID, publication, callback) {
-		await Publication.findByIdAndUpdate(publicationID, publication, callback)
-	}
+    if (publication == null) {
+      throw new ArrayError([{ msg: "No publications found for this user" }]);
+    }
+
+    return publication;
+  }
+
+  async getPostAnalytics(publicationID, userID) {
+    const analytics = await Publication.getPostAnalyticsByDate({
+      _id: publicationID,
+      user_id: userID,
+    });
+    return analytics;
+  }
+
+  async updatePublication(publicationID, publication, callback) {
+    await Publication.findByIdAndUpdate(publicationID, publication, callback);
+  }
 }
 
-module.exports = PublicationService
+module.exports = PublicationService;
